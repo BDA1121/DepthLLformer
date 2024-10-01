3473d0d167b3c771a3eeb0442266cf100803cd09
Dhanush Adithya Balamurugan <59655416+BDA1121@users.noreply.github.com>
2024-10-01 11:55:36 -0400

    Update pipeline.py

--- metric_depth/pipeline.py
+++ metric_depth/pipeline.py
@@ -1,4 +1,6 @@
-## Modified By- Harshil Bhojwani and Dhanush Adithya
+## created by Harshil Bhojwani and Dhanush Adithya
+## CS 7180 Advanced Perception
+## 09/30/2024
 
 import cv2
 import glob
@@ -73,51 +75,3 @@ def depth_estimation(x, model,input_size= 128, pred_only=False, grayscale=False)
       return depth_tensor
   else:
       return depth_tensor
-
-# Example usage:
-# Assuming x is your input tensor with shape [32, 3, H, W]
-# model = load_model()  # Load your model
-# result = depth_estimation(x, model)
-# print(result.shape)  # Should be [32, 3, H, W] or [32, 1, H, W] if grayscale
-        #   split_region = np.ones((raw_image.shape[1], 50, 3), dtype=np.uint8) * 255
-        #   combined_result = cv2.hconcat([raw_image, split_region, depth])
-        #   cv2.imwrite(output_path_image, depth)   
-        #   return depth     
-#   for k, filename in enumerate(filenames):
-#       print(f'Progress {k+1}/{len(filenames)}: {filename}')
-      
-#       raw_image = cv2.imread(filename)
-      
-#       if raw_image is None:
-#           print(f"Error: Unable to read image {filename}")
-#           continue
-      
-      # Convert image to tensor and move to GPU if available
-    #   raw_image_tensor = torch.from_numpy(raw_image).float().permute(2, 0, 1).unsqueeze(0).to(DEVICE)
-      
-    #   depth = depth_anything.infer_image(raw_image_tensor, input_size)
-      
-    #   if save_numpy:
-    #       output_path = os.path.join(outdir, os.path.splitext(os.path.basename(filename))[0] + '_raw_depth_meter.npy')
-    #       np.save(output_path, depth.cpu().numpy())
-      
-    #   depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
-    #   depth = depth.cpu().numpy().astype(np.uint8)  # Move back to CPU for further processing
-      
-    #   if grayscale:
-    #       depth = np.repeat(depth[..., np.newaxis], 3, axis=-1)
-    #   else:
-    #       depth = (cmap(depth)[:, :, :3] * 255)[:, :, ::-1].astype(np.uint8)
-      
-    #   output_path = os.path.join(outdir, os.path.splitext(os.path.basename(filename))[0] + '.png')
-    #   output_path_image = os.path.join(outdir, os.path.splitext(os.path.basename(filename))[0] + (model if model else '') + '.png')
-    #   if pred_only:
-    #       cv2.imwrite(output_path, depth)
-    #   else:
-    #       split_region = np.ones((raw_image.shape[1], 50, 3), dtype=np.uint8) * 255
-    #       combined_result = cv2.hconcat([raw_image, split_region, depth])
-    #       cv2.imwrite(output_path_image, depth)            
-          # cv2.imwrite(output_path, combined_result)
-
-# Example usage:
-# depth_estimation('path/to/image_or_directory', input_size=518, outdir='./vis_depth', encoder='vitl', load_from='checkpoints/depth_anything_v2_metric_hypersim_vitl.pth', max_depth=20, save_numpy=False, pred_only=False, grayscale=False, model='outdoorL')
